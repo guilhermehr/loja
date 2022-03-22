e4b461722c1fe843fd6aa60b0905b1b7dd249b74
Guilherme Henrique Ribeiro <guilherme.h.ribeiro@bradesco.com.br>
2022-03-21 22:40:23 -0300

    correcoes e eureka

--- src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,5 +1,6 @@
 package br.com.alura.microservice.loja.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +15,13 @@ public class CompraService {
 
 	@Value("${url.service.fornecedor}")
 	private String urlMicroservicoFornecedor;
+	
+	@Autowired
+	private RestTemplate client;
 
 	public void realizaCompra(CompraDTO compra) {
-		RestTemplate client = new RestTemplate();
-		ResponseEntity<InfoFornecedorDTO> exchange = client.exchange(
+
+		ResponseEntity<InfoFornecedorDTO> exchange = this.client.exchange(
 				this.urlMicroservicoFornecedor + "/info/" + compra.getEndereco().getEstado(), HttpMethod.GET, null,
 				InfoFornecedorDTO.class);
 		
